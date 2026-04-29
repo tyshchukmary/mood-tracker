8543cae80a82a2556d5bf4444685f241f25387f5
Maria <maria@MacBook-Air-Maria.local>
2026-04-30 00:41:40 +0300

    fix: add path alias and tailwind config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // Додай цей імпорт
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path' // Додай цей імпорт
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // Додай це в список плагінів
+    tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      // Це налаштування виправить помилку з "@/"
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
